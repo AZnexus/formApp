1c309a856299855347ed7a00f1b274170dd3ad98
Alex Zarauza <azarauza@nexusgeographics.com>
2024-03-19 10:50:07 +0100

    Validator Service

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import * as customValidators from 'src/app/shared/validators/validators'; // import all amb un nom de variable per poder accedir des de dins el codi.
+import { ValidatorsService } from 'src/app/shared/services/validators.service';
+// import * as customValidators from 'src/app/shared/validators/validators'; // import all amb un nom de variable per poder accedir des de dins el codi.
 
 @Component({
   templateUrl: './register-page.component.html',
@@ -10,17 +11,22 @@ import * as customValidators from 'src/app/shared/validators/validators'; // imp
 export class RegisterPageComponent {
 
   public myForm: FormGroup = this.fb.group({
-    name: ['', [Validators.required, Validators.pattern(customValidators.firstNameAndLastnamePattern)]],
-    email: ['', [Validators.required, Validators.pattern(customValidators.emailPattern)]], // Afegeix un pattern custom amb els validators
-    username: ['', [Validators.required, customValidators.cantBeStrider]],
+    // name: ['', [Validators.required, Validators.pattern(customValidators.firstNameAndLastnamePattern)]],
+    // email: ['', [Validators.required, Validators.pattern(customValidators.emailPattern)]], // Afegeix un pattern custom amb els validators
+    // username: ['', [Validators.required, customValidators.cantBeStrider]],
+    name: ['', [Validators.required, Validators.pattern(this.validatorsService.firstNameAndLastnamePattern)]],
+    email: ['', [Validators.required, Validators.pattern(this.validatorsService.emailPattern)]],
+    username: ['', [Validators.required, this.validatorsService.cantBeStrider]],
     password: ['', [Validators.required, Validators.minLength(6)]],
     password2: ['', [Validators.required]]
   });
 
-  constructor(private fb: FormBuilder) {}
+  constructor(
+    private fb: FormBuilder,
+    private validatorsService: ValidatorsService) {}
 
   isValidField(field: string) {
-    //TODO: Obtenerlo de un servicio especializado
+    return this.validatorsService.isValidField(this.myForm, field);
   }
 
   onSubmit() {
